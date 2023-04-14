1f20a2385f1f2e7e7b2e1962ec258d775aa0941b
The LEGEND <87276808+gguisales@users.noreply.github.com>
2023-04-14 10:17:36 -0300

    Add files via upload

--- DS/index.php
+++ DS/index.php
@@ -1,64 +1,7 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desenvolvimento de Sistemas</title>
-    <link rel="shortcut icon" href="icon.png" type="image/x-icon">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    
-  <header>
-    <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Produtos</a></li>
-        <li><a href="#">Sobre</a></li>
-        <li><a href="#">Contato</a></li>
-      </ul>
-    </nav>
-  </header>
+<?php get_header(); ?>
 
-  <main>
-    <section class="banner">
-      <h1>Compre o melhor software</h1>
-      <p>Encontre o software certo para suas necessidades</p>
-    </section>
+   <h1 class="first">kdajjskj</h1>
 
-    <section class="produtos">
-      <h2>Produtos em Destaque</h2>
-      <ul>
-        <li>
-          <img src="imagem-software-1.jpg" alt="Software 1">
-          <h3>Software 1</h3>
-          <p>Descrição do software 1.</p>
-          <button>Comprar</button>
-        </li>
-        <li>
-          <img src="imagem-software-2.jpg" alt="Software 2">
-          <h3>Software 2</h3>
-          <p>Descrição do software 2.</p>
-          <button>Comprar</button>
-        </li>
-        <li>
-          <img src="imagem-software-3.jpg" alt="Software 3">
-          <h3>Software 3</h3>
-          <p>Descrição do software 3.</p>
-          <button>Comprar</button>
-        </li>
-      </ul>
-    </section>
+<?php get_footer(); ?>
 
-    <section class="sobre">
-      <h2>Sobre Nós</h2>
-      <p>Texto sobre a empresa e seus valores.</p>
-    </section>
-  </main>
-
-  <footer>
-    <p>&copy; 2023 Venda de Software. Todos os direitos reservados.</p>
-  </footer>
-</body>
-</html>
+ 
